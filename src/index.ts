8b1b8ea49fac69650c5c59db646114ed89385303
HelixHEX <e.wambugu192@gmail.com>
2021-03-22 20:32:13 -0400

    first commit

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import "dotenv-safe/config";
+import "reflect-metadata";
+
+import express from "express";
+
+const cors = require("cors");
+import morgan from "morgan";
+
+const main = () => {
+  const app = express();
+
+  app.use(morgan("dev"));
+
+  app.use(cors({ origin: "*" }));
+
+  app.use(express.json());
+
+  app.get("/", (_, res: express.Response) => {
+    res.send("Hello world");
+  });
+
+  app.use((_, res: express.Response) => {
+    res.status(404).json({ status: "404" });
+  });
+
+  app.listen(process.env.PORT, () => {
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}`);
+  });
+};
+
+main();
